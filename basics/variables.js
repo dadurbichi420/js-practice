e32b12631857040be976cc2c3519e6c7c7f7728d
dadurbichi420 <163672067+dadurbichi420@users.noreply.github.com>
2026-08-05 11:34:55 +0000

    refactor variable assignments and clean up console output

--- basics/variables.js
+++ basics/variables.js
@@ -4,8 +4,8 @@ var accountPassword = "1234"
 accountCity = "Kolkata"
 
 //accountID = 56987
-let accountEmail = "Xoxo@123"
-var accountPassword = "456987"
+accountEmail = "Xoxo@123"
+accountPassword = "456987"
 accountCity = "Chennai"
 
-console.table([accountID,accountEmail,accountPassword,accountCity]);
+console.table([accountID,accountEmail,accountPassword,accountCity])
