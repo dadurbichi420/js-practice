bde48e73c2862cde2846cc41c87ac3204c637857
dadurbichi420 <163672067+dadurbichi420@users.noreply.github.com>
2026-08-05 12:32:03 +0000

    add boolean conversion examples and log results to console

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -14,3 +14,19 @@ null -> 0
 "33abc" -> NaN
 undefined -> NaN
 */
+
+let IsLoggedIn= "jojo"
+let IsLoggedInNumber= Boolean(IsLoggedIn)
+console.log(typeof IsLoggedIn)
+console.log(typeof IsLoggedInNumber)
+console.log(IsLoggedInNumber)
+
+/*
+1 -> true
+0 -> false
+" " -> false
+"jojo" -> true
+null -> false
+undefined -> false
+NaN -> false
+*/
