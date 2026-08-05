49355b4f60680c845ab91b70459f3f1f00ad50ef
dadurbichi420 <163672067+dadurbichi420@users.noreply.github.com>
2026-08-05 12:25:01 +0000

    add conversion operation examples and type checks

---
+++ basics/conversionOperation.js
@@ -0,0 +1,16 @@
+let score= false
+let j=null
+let valueInNumber=Number(score)
+console.log(typeof score)
+console.log(typeof j)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+"33" -> 33
+true -> 1
+false -> 0
+null -> 0
+"33abc" -> NaN
+undefined -> NaN
+*/
