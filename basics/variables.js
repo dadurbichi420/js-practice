a626f1d76a6e878888a552dc21f60271c422feb0
dadurbichi420 <163672067+dadurbichi420@users.noreply.github.com>
2026-08-05 11:17:11 +0000

    add variables for account details and log to console

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountID = 123456
+let accountEmail = "Jojo@123"
+var accountPassword = "1234"
+accountCity = "Kolkata"
+
+//accountID = 56987
+let accountEmail = "Xoxo@123"
+var accountPassword = "456987"
+accountCity = "Chennai"
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
