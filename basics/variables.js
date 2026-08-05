27f855da59c90032c0523f2c67e80b32a21ddebf
dadurbichi420 <163672067+dadurbichi420@users.noreply.github.com>
2026-08-05 11:35:53 +0000

    add accountState variable and include it in console output

--- basics/variables.js
+++ basics/variables.js
@@ -2,10 +2,11 @@ const accountID = 123456
 let accountEmail = "Jojo@123"
 var accountPassword = "1234"
 accountCity = "Kolkata"
+let accountState
 
 //accountID = 56987
 accountEmail = "Xoxo@123"
 accountPassword = "456987"
 accountCity = "Chennai"
 
-console.table([accountID,accountEmail,accountPassword,accountCity])
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
